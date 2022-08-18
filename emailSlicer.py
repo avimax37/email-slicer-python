149f0b029a46e990ed101f750c2680797f3bd3b6
Avinaba Bera <102454842+avimax37@users.noreply.github.com>
2022-08-18 11:46:15 +0530

    Update emailSlicer.py

--- emailSlicer.py
+++ emailSlicer.py
@@ -4,8 +4,8 @@ email = input().strip()
 if email.find("@") != -1:
     username = email[:email.index("@")]     # username value = from 0 to index < @
     domain = email[email.index("@") + 1:]   # domain value = from index > @ to end
-    print("Your userame is: ", username)
-    print("Your domain name is: ", domain)
+    print("Your username is: ", username)
+    print("Your domain is: ", domain)
 else:
     print("Please enter a valid Email Id.")
 
